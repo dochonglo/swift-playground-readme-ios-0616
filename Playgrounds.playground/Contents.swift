497bca1b37c27016307d59ce7e0d0c901da94d34
dochonglo <ismaelbarry777@hotmail.com>
2016-07-05 12:55:08 -0400

    Playground Files Lab is Complete

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,9 @@
 let str = "Hello, world"
-str = "Goodbye, world"
+
+//str = "Goodbye, world" // This will not work because we defined str as a constant, and constans cannot be changed.
+
+// However, we can do this:
+
+var str2 = "Hello, world"
+
+str2 = "Good bye, world"
